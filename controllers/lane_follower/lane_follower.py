6076efd987674576784cfa12e37f90aafcde809b
Darko Lukić <lukicdarkoo@gmail.com>
2021-01-11 12:14:43 +0100

    Update lane_follower.py

--- controllers/lane_follower/lane_follower.py
+++ controllers/lane_follower/lane_follower.py
@@ -9,7 +9,7 @@ SHOW_IMAGE_WINDOW = False
 
 def on_cv_image_click_event(event, x, y, _, img):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(img[y, x])
+        print('You clicked on a pixel with color:', img[y, x])
 
 
 def regulate(tesla, camera):
@@ -22,13 +22,13 @@ def regulate(tesla, camera):
     img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     mask = cv2.inRange(img, np.array([70, 120, 170]), np.array([120, 160, 210]))
 
-    # Find the largest segmented contour (red ball) and it's center
+    # Find the largest segmented contour
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     largest_contour = max(contours, key=cv2.contourArea)
     largest_contour_center = cv2.moments(largest_contour)
     center_x = int(largest_contour_center['m10'] / largest_contour_center['m00'])
 
-    # Find error (ball distance from image center)
+    # Find error (the lance distance from target)
     error = center_x - 120
     tesla.setSteeringAngle(error * CONTROL_COEFFICIENT)
 
@@ -41,6 +41,8 @@ def regulate(tesla, camera):
 def main():
     tesla = Driver()
     timestep = int(tesla.getBasicTimeStep())
+    
+    # Init camera
     camera = tesla.getDevice('camera')
     camera.enable(timestep)
     camera.recognitionEnable(timestep)
